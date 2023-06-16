b644f40071f0efc04d8c61ed4ef1e282e795b9f0
Vitor <vitorsirotenco33@gmail.com>
2023-06-15 22:51:15 -0300

    Implemento do jogo da Forca

--- forca.py
+++ forca.py
@@ -4,5 +4,30 @@ def jogar():
     print('**Bem-vindo ao jogo da forca!***')
     print('********************************')
 
+    palavra_secreta = 'banana'
+    letras_acertadas = ['_','_','_','_','_','_',]
+
+    enforcou = False
+    acertou = False
+
+    #enquanto (true)
+    while (not enforcou and not acertou):
+
+        chute = input('Qual letra? \n').strip() #pra desconsiderar espaços na letra
+
+        posicao = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()): #para comparar sempre entre letras maisculas
+                letras_acertadas[posicao] = letra
+                break
+            elif(chute.upper() != letra.upper()):
+                print('Não tem essa letra')
+                break
+
+            posicao = posicao +1
+
+        print(letras_acertadas)
+
+
 if(__name__ == '__main__'): #para chamar a função e rodar o jogo sem dar erro no arquivo jogos
     jogar()
---
+++ teste.py
@@ -0,0 +1,4 @@
+frase = "Olá, mundo!"
+
+for i in range(len(frase)):
+    print("O caractere na posição", i, "é", frase[i])
